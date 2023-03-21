31cf8f2f8577c2b70166bd3317642d0d5d1ca7b7
EuropeHP <seowhanphing@hotmail.com>
2023-03-21 09:06:15 +0530

    user2 updated line 17 in Catalog.cs

--- CatalogAPI/Models/Catalog.cs
+++ CatalogAPI/Models/Catalog.cs
@@ -14,6 +14,7 @@ namespace CatalogAPI.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required]
+	//user2 updated on 8.45am
         [Column("Catalog_Id")]
         public long CatalogId { get; set; }
         [Column("Catalog_Name")]
